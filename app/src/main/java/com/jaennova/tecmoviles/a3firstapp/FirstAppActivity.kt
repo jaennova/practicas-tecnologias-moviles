39b65401158ac0ea3eba40e9dfbed655e79799b7
Jaen <jaennova9@gmail.com>
2024-04-08 15:54:27 -0600

    update menu

--- app/src/main/java/com/jaennova/tecmoviles/a3firstapp/FirstAppActivity.kt
+++ app/src/main/java/com/jaennova/tecmoviles/a3firstapp/FirstAppActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -14,8 +13,10 @@ class FirstAppActivity : AppCompatActivity() {
 
     private lateinit var etName: EditText
     private lateinit var btnChangeActivity: Button
-    private lateinit var tvName: TextView
 
+    companion object {
+        const val EXTRA_NAME = "EXTRA_NAME"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -28,8 +29,6 @@ class FirstAppActivity : AppCompatActivity() {
     private fun initializeViews() {
         etName = findViewById(R.id.etName)
         btnChangeActivity = findViewById(R.id.btnChangeActivity)
-        tvName = findViewById(R.id.tvName)
-        tvName.text = intent.getStringExtra("name")
     }
 
     private fun setListeners() {
@@ -39,19 +38,19 @@ class FirstAppActivity : AppCompatActivity() {
     private fun checkValue() {
         val name = etName.text.toString()
         if (name.isEmpty()) {
-            showToast("el nombre no puede estar vacio")
+            "el nombre no puede estar vacio".showToast()
         } else {
             startResultActivity(name)
         }
     }
 
-    private fun showToast(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    private fun String.showToast() {
+        Toast.makeText(this@FirstAppActivity, this, Toast.LENGTH_SHORT).show()
     }
 
     private fun startResultActivity(name: String) {
         val intent = Intent(this, ResultActivity::class.java)
-        intent.putExtra("name", name)
+        intent.putExtra(EXTRA_NAME, name)
         startActivity(intent)
     }
 }
--- app/src/main/java/com/jaennova/tecmoviles/a3firstapp/ResultActivity.kt
+++ app/src/main/java/com/jaennova/tecmoviles/a3firstapp/ResultActivity.kt
@@ -6,23 +6,23 @@ import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.jaennova.tecmoviles.R
+import com.jaennova.tecmoviles.a3firstapp.FirstAppActivity.Companion.EXTRA_NAME
 
 class ResultActivity : AppCompatActivity() {
     private lateinit var btnBack: Button
-    private lateinit var tvGreeting: TextView
+    private lateinit var tvResult: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_result)
         btnBack = findViewById(R.id.btnBack)
-
         getAndShowName()
         btnBack.setOnClickListener { onBackPressed() }
     }
-    private fun getAndShowName(){
-        tvGreeting = findViewById(R.id.tvGreeting)
-        val bundle = intent.extras
-        val name = bundle?.get("name")
-        tvGreeting.text = getString(R.string.welcome, name)
+
+    private fun getAndShowName() {
+        tvResult = findViewById(R.id.tvResult)
+        val name: String = intent.extras?.getString(EXTRA_NAME).orEmpty()
+        tvResult.text = getString(R.string.welcome, name)
     }
 }
